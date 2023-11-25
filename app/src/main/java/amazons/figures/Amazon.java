999684c47d3bc2a745b0e0b7a15819f3d2c38f33
Mahoui <mahouisonia55@gmail.com>
2023-11-25 17:46:48 +0100

    création de la class Amazon

--- app/src/main/java/amazons/figures/Amazon.java
+++ app/src/main/java/amazons/figures/Amazon.java
@@ -1,4 +1,34 @@
 package amazons.figures;
 
-public class Amazon {
+import amazons.board.Board;
+import amazons.board.Position;
+import amazons.player.PlayerID;
+
+import java.util.List;
+
+public class Amazon extends MovableFigure implements Figure{
+    @Override
+    public boolean canMoveTo(Position position, Board board) {
+        return false;
+    }
+
+    @Override
+    public void moveTo(Position position, Board board) {
+
+    }
+
+    @Override
+    public void setPosition(Position position) {
+
+    }
+
+    @Override
+    public PlayerID getPlayerID() {
+        return null;
+    }
+
+    @Override
+    public List<Position> getAccessiblePositions(Board board) {
+        return null;
+    }
 }
