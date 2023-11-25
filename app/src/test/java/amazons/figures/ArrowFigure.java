719435f7fad0343be4e998dfae3df90a431bb129
Mahoui <mahouisonia55@gmail.com>
2023-11-25 17:23:14 +0100

    fin de la creation de classe + attribut et constructeur (tache4)

---
+++ app/src/test/java/amazons/figures/ArrowFigure.java
@@ -0,0 +1,29 @@
+package amazons.figures;
+
+import amazons.board.Board;
+import amazons.board.Position;
+import amazons.player.PlayerID;
+
+public class ArrowFigure implements Figure{
+    public static final ArrowFigure ARROW_FIGURE = new ArrowFigure();
+    private ArrowFigure(){}
+    @Override
+    public boolean canMoveTo(Position position, Board board) {
+        return false;
+    }
+
+    @Override
+    public void moveTo(Position position, Board board) {
+
+    }
+
+    @Override
+    public void setPosition(Position position) {
+
+    }
+
+    @Override
+    public PlayerID getPlayerID() {
+        return null;
+    }
+}
