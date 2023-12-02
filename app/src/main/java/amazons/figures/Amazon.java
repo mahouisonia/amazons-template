10aa490ec1b20a9865a0f03d855cfee78e2ab58d
Mahoui <mahouisonia55@gmail.com>
2023-12-02 15:36:09 +0100

    implémentation de getAccessiblePositions

--- app/src/main/java/amazons/figures/Amazon.java
+++ app/src/main/java/amazons/figures/Amazon.java
@@ -1,12 +1,17 @@
 package amazons.figures;
 
 import amazons.board.Board;
+import amazons.board.CardinalDirection;
+import amazons.board.MatrixBoard;
 import amazons.board.Position;
 import amazons.player.PlayerID;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Amazon extends MovableFigure implements Figure{
+    Position position;
+    public Amazon(Position position){ this.position = position;}
     @Override
     public boolean canMoveTo(Position position, Board board) {
         return false;
@@ -29,6 +34,34 @@ public class Amazon extends MovableFigure implements Figure{
 
     @Override
     public List<Position> getAccessiblePositions(Board board) {
+        List<Position> accessiblePositions = new ArrayList<>();
+
+        for (CardinalDirection direction : CardinalDirection.values()) {
+            Position currentPosition = position;
+
+            while (true) {
+                Position nextPosition = currentPosition.next(direction);
+
+                if (board.isOutOfBoard(nextPosition)
+                        || !board.isEmpty(nextPosition)) {
+                    break;
+                }
+                accessiblePositions.add(nextPosition);
+                currentPosition = nextPosition;
+            }
+        }
+
+        return accessiblePositions;
+    }
+
+
+
+
+
+
+
+
+
         return null;
     }
 }
